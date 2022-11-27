57b9f1b674fa15414a5292e0a216f7e3b28a6f43
agcarvalho-eng <agcarvalho@ifto.edu.br>
2022-11-27 16:18:24 -0300

    Javascript1

--- javascript/ifElse.js
+++ javascript/ifElse.js
@@ -1,17 +1,27 @@
 const numero = prompt("Digite um número inteiro e tecle entrer.");
-    if(numero > 0){
+if(numero > 0){
+    if(numero % 2 == 0){
+        alert("O número "+numero+" é positivo e par.");
+    }
+    else{
+        alert("O número "+numero+" é positivo e ímpar.");
+    }
+}
+else{
+    if(numero < 0){
         if(numero % 2 == 0){
-            alert("O número "+numero+" é positivo e par.");
+            alert("O número "+numero+ " é negativo e par.");
         }
         else{
-            alert("O número "+numero+" é positivo e ímpar.");
+            alert("O número "+numero+" é negativo e ímpar.");
         }
     }
     else{
         if(numero == 0){
-            alert("O número "+numero+" possui valor zero.");
+            alert("O número "+numero+" tem o valor zero.");
         }
         else{
-                alert("O número "+numero+" é negativo");
+            alert("Operação cancelada pelo usuário.");
         }
     }
+}
